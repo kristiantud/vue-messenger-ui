3cca5f77d4d9ad041297bb0c057bf7963653233e
Kristian Tud <kristiantud@hotmail.com>
2023-08-12 16:41:46 -0600

    trying to recreate facebook messenger in vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import MessagesWidget from './components/MessagesWidget.vue';
+</script>
+
+<template>
+    <MessagesWidget />
+</template>
+
+
+<style scoped>
+body{
+  background-color: black;
+}
+</style>
---
+++ src/components/MessagesWidget.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+    <div style="height: 700px; width: 1300px; background-color: #202020;  margin: auto; margin-top: 50px; border-radius: 10px; display: flex;">
+        <div style="overflow: auto; width: 600px;">
+
+        </div>
+        <div style="width: 100%;">
+
+        </div>
+    </div>
+
+
+</template>
+
+
